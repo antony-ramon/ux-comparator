05feaec32afc9f08389d4dff888910bed878b82d
Camille Sauer <camille-sauer@hotmail.fr>
2020-05-26 12:04:22 +0200

    création de model

---
+++ app/models/companie.rb
@@ -0,0 +1,2 @@
+class Companie < ApplicationRecord
+end
---
+++ test/models/companie_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CompanieTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
