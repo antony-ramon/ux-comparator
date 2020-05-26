960f5710fdca923ffbadcb65e8765dc06812ecd9
Lucille James <lucille.james00@gmail.com>
2020-05-26 12:20:39 +0200

    fixed column issues

--- db/migrate/20200526093711_create_typologies.rb
+++ db/migrate/20200526093711_create_typologies.rb
@@ -3,7 +3,6 @@ class CreateTypologies < ActiveRecord::Migration[6.0]
     create_table :typologies do |t|
       t.string :title
       t.string :process_name
-      t.integer :field_id
 
       t.timestamps
     end
--- db/migrate/20200526094448_create_user_fields.rb
+++ db/migrate/20200526094448_create_user_fields.rb
@@ -1,8 +1,6 @@
 class CreateUserFields < ActiveRecord::Migration[6.0]
   def change
     create_table :user_fields do |t|
-      t.integer :user_id
-      t.integer :field_id
 
       t.timestamps
     end
