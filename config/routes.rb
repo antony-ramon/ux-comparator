bab4b7db8a8093c7596abf7a27cbe80edd266c11
Antony RAMON <61970627+antony-ramon@users.noreply.github.com>
2020-05-28 15:04:42 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
       resources :indicators, only: [:index]
     end
   end
-  resources :articles, only: [:index]
+  resources :articles, only: [:index, :show]
   get '/dashboard', to: 'dashboard#activity', as: :dashboard
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
