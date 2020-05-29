8208722c136df8f8d9d93708f75ebf636db02a22
Antony RAMON <github@mybluemails.com>
2020-05-29 11:40:01 +0200

    Raw draft on bookmarked items page

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,13 @@
 class DashboardsController < ApplicationController
   def activity
   end
+  def bookmarks
+    @user = current_user
+    authorize(:Dashboard)
+    @articles = policy_scope(Article)
+    # @articles = Article.all
+    @articles = @user.articles_liked
+    # @article = @user.likes @articles.last
+    @typologies = Typology.all.sort_by { |typology| [typology.field.name, typology.process_name] }
+  end
 end
