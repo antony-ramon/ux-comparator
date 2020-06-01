2f245d3c5daa0b8e8a36ec86f82f396ca2e1a23c
Antony RAMON <github@mybluemails.com>
2020-06-01 12:35:13 +0200

    Search bar

--- app/controllers/typologies_controller.rb
+++ app/controllers/typologies_controller.rb
@@ -1,9 +1,10 @@
 class TypologiesController < ApplicationController
   def index
-  @field = Field.find(params[:field_id])
-  # @typologies = @field.typologies.where(process_name: params[:process])
-  # @field = field_002
-  @process_name = "Subscription"
-  @typologies = policy_scope(Typology).where(process_name: @process_name)
+    # @field = Field.find(params[:field_id])
+    # @field = Field.where(name: params[:field])
+    # @typologies = @field.typologies.where(process_name: params[:process])
+    # @field = field_002
+    # @process_name = "Subscription"
+    @typologies = policy_scope(Typology).where(process_name: @process_name)
   end
 end
