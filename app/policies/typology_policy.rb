a6acdc61cef1707bdf8d5581ada2d8bc6c1a6f8c
Antony RAMON <github@mybluemails.com>
2020-06-01 18:18:46 +0200

    Like benchmark

--- app/policies/typology_policy.rb
+++ app/policies/typology_policy.rb
@@ -4,4 +4,24 @@ class TypologyPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def create?
+    user.admin
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def like?
+    true
+  end
+
+  def unlike?
+    true
+  end
 end
